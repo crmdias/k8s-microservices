c6d72bae6294c98f183953a05c936c35e2b9451e
Cristiano Dias <i867680@slel00681834a.amer.global.corp.sap>
2018-09-26 18:58:28 +0200

    Implements mongo connection, /user/route now works

--- user_service.py
+++ user_service.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import pymongo
 import jwt
 app = Flask(__name__)
 
@@ -10,7 +11,20 @@ db = client.get_default_database()
 
 @app.route("/user/auth")
 def user_auth():
-    #se user existe com essas credenciais
-    return jwt.encode({'user_id': user.id}, '31AB8F2718655495D5347A325FA9A', algorithm='HS256')
-    #se nao
-    #403 na tua cara
+    username = request.headers.get('username')
+    password = request.headers.get('password')
+    if not username:
+        return "{\"message\": \"Bad request: username missing\"}", 400
+    if not password:
+        return "{\"message\": \"Bad request: password missing\"}", 400
+
+    try:
+        user = db['workshop_users'].find({'username': username, 'password': password})[0]
+    except Exception as e:
+        print(e)
+        return "{\"message\": \"Unable to authenticate with the given user\"}", 403
+
+    return jwt.encode({'user_id': user["id"]}, '31AB8F2718655495D5347A325FA9A', algorithm='HS256'), 200
+    
+if __name__ == "__main__":
+    app.run()
